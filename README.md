d5d83ef90595c2178a2c9ec5e0d742d6207150ed
zhuzidian <zhuzidian@gmail.com>
2022-07-29 15:58:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# bcrypt-generator
+
+## コンパイル
+
+### M1 MACでwindows用のexeをコンパイル
+
+```bash
+CGO_ENDABLED=0 GOOS=windows GOARCH=amd64 go build
+```
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	inputFile := flag.String("input", "pw.txt", "明文パスワード")
+	outputFile := flag.String("output", "pw-hash.txt", "暗号化パスワード")
+	cost := flag.Int("cost", 4, "暗号化回数")
+	flag.Parse()
+
+	// fmt.Println(*inputFile)
+	// fmt.Println(*outputFile)
+	// fmt.Println(*cost)
+
+	bytes, err := os.ReadFile(*inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	plainPasswordArray := strings.Split(string(bytes), "\n")
+
+	var hashPasswordArray []string
+	for _, password := range plainPasswordArray {
+		if strings.TrimSpace(password) == "" {
+			continue
+		}
+		hash, err := bcrypt.GenerateFromPassword([]byte(strings.TrimSpace(password)), *cost)
+		if err != nil {
+			log.Fatal(err)
+		}
+		hashPasswordArray = append(hashPasswordArray, string(hash))
+	}
+
+	os.WriteFile(*outputFile, []byte(strings.Join(hashPasswordArray, "\n")), 0644)
+}
